97d9ebd5db926fbcb94c229ceafa58852ff6c36a
JohnnyKodes <johnnykaram@protonmail.com>
2022-03-01 19:45:26 +0200

    Fixed typo in Coin Details page time selector from 3m to 3M

--- src/components/CoinDetails/index.jsx
+++ src/components/CoinDetails/index.jsx
@@ -28,7 +28,7 @@ const CoinDetails = () => {
 
   const cryptoDetails = data?.data?.coin;
 
-  const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
+  const time = ["3h", "24h", "7d", "30d", "1y", "3M", "3y", "5y"];
 
   if (isFetching) return <Loader />;
 
